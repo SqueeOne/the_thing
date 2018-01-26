29b88e40b8025cd30cd1d6bd100baf45c622e083
Squee <selih.design@gmail.com>
2018-01-26 12:23:17 +0000

    Added rules for time division form.

--- user_admin/static/user_admin/form_calc.js
+++ user_admin/static/user_admin/form_calc.js
@@ -1,20 +1,24 @@
-jQuery('.divide-form').on('change keyup check', '.form-control', function() {
+jQuery('.divide-form').on('change', '.form-control', function() {
     var sum = 0;
+    var timeLeft = parseFloat(jQuery('.hours-worked').text());;
     var $time = parseFloat(jQuery('.hours-worked').text());
-    var $alertdiv = jQuery("<div class='alert alert-danger'><p>Boooo hooo hooo</p></div>");
+    var $alertdiv = jQuery("<div class='alert alert-danger'><p>Število vnešenih ur je višje od količine oddelanih ur, prosim popravite znesek.</p></div>");
     jQuery('.form-control').each(function(){
         sum += +jQuery(this).val();
+        timeLeft -= +jQuery(this).val();
+        jQuery('.time-left').text(timeLeft).toString();
         if(sum>$time) {
             jQuery('.form-control').css('background-color', "red");
             jQuery('.btn').prop('disabled', true);
-            
             jQuery('.alertz').append($alertdiv);
+        } else if(sum<$time) {
+            jQuery('.btn').prop('disabled', true);
+            jQuery('.form-control').css('background-color', "#ffffff");
+            jQuery('.alert').remove();
         } else {
             jQuery('.btn').prop('disabled', false);
             jQuery('.form-control').css('background-color', "#ffffff");
+            jQuery('.alert').remove();
         }
     });
-    
 });
-
-
